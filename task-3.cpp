c81dbef78487484edf6a6110b0cb6445b2ea3d81
Воробьёв Владислав <72572241+VorobyovVV@users.noreply.github.com>
2020-12-15 20:13:18 +0500

    Add files via upload

---
+++ task-3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::string s, f1_name, f2_name;
+    f1_name = "data.txt";
+    f2_name = "newdata.txt";
+    try {
+        std::ifstream inFile(f1_name);
+        std::ofstream toFile(f2_name);
+        if (!inFile) throw "Can not read file";
+        if (!toFile) throw "Can not create file";
+
+        while (inFile.peek() != EOF)
+        {
+            getline(inFile, s);
+            toFile << s << std::endl;
+        }
+        inFile.close();
+    }
+    catch (const char* error) {
+        std::cout << "Error! " << error << "!" << std::endl;
+        system("pause");
+        return 1;
+    }
+    return 0;
+}
