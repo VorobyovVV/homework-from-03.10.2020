ffafc1402f1886cfe027b0da5f69b710fa45ec85
Воробьёв Владислав <72572241+VorobyovVV@users.noreply.github.com>
2020-12-15 17:25:02 +0500

    Add files via upload

---
+++ Task - 1 (2).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+double fx(double x) {
+	return 2 * x - 1; 
+}
+double derivative(double x) {
+	return 2; 
+}
+typedef double(*fun)(double x);
+double fsolve(fun fx, fun derivative, double x0) {
+	double x1 = x0 - fx(x0) / derivative(x0);
+	while (fabs(x1 - x0) > 0.000001) {
+		x0 = x1;
+		x1 = x0 - fx(x0) / derivative(x0);
+	}
+	return x1;
+}
+
+int main() {
+	std::cout << fsolve(fx, derivative, 10);
+	return 0;
+}
